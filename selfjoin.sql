e4e2b3b5605e801a836b6a78470fbad6423e2a0f
vivian0228-code <vivianximingcheng@gmail.com>
2026-02-02 11:58:36 +0800

    Update selfjoin.sql

--- selfjoin.sql
+++ selfjoin.sql
@@ -6,3 +6,57 @@ FROM route WHERE stop=149 OR stop=53
 GROUP BY company, num
 HAVING COUNT(*)=2
 
+
+/*
+Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+*/
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
+
+
+
+/*
+The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. 
+*/
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
+
+
+/*
+Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+*/
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE a.stop=115 AND b.stop=137
+
+
+
+/*
+Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+*/
+SELECT DISTINCT stopa.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE a.company='LRT' AND a.num IN (SELECT num FROM route JOIN stops ON route.stop=stops.id WHERE stops.name='Craiglockhart')
+
+
+/*
+10.
+Find the routes involving two buses that can go from Craiglockhart to Lochend.
+Show the bus no. and company for the first bus, the name of the stop for the transfer,
+and the bus no. and company for the second bus.
+*/
+
+
+
